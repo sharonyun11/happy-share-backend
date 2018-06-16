babd9a9587c5527858b17cf3f04fa777c50b26c1
Sharon Yun <sharonyun11@gmail.com>
2018-06-15 20:10:44 -0400

    modified app to include proper callback

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const init = () => {
   db.sync()
   app.listen(PORT, () => {
     console.log(
-      `Listening on port ${8888}. Go /login to initiate authentication flow.`
+      `Listening on port ${PORT}. Go /login to initiate authentication flow.`
     )
   })
 }
